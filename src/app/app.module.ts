ca380719b558fba4c4e357f0e6ef27bdd9f367bc
Nikita <laumiqv@gmail.com>
2020-04-16 15:31:26 +0300

    translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,124 +1,184 @@
-import {BrowserModule, makeStateKey, StateKey, TransferState} from '@angular/platform-browser';
-import {APP_INITIALIZER, NgModule, PLATFORM_ID, Injector} from '@angular/core';
+import {
+  BrowserModule,
+  makeStateKey,
+  StateKey,
+  TransferState,
+} from "@angular/platform-browser";
+import {
+  APP_INITIALIZER,
+  NgModule,
+  PLATFORM_ID,
+  Injector,
+} from "@angular/core";
 
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
-import {HttpClient, HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateService,
+} from "@ngx-translate/core";
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpClientXsrfModule,
+} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './index/header/header.component';
-import { StartFormComponent } from './index/start-form/start-form.component';
-import { IndexComponent } from './index/index.component';
-import { WillComponent } from './will/index.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ContractEditResolver, ContractFormComponent} from './contract-form/contract-form.component';
-import {MatNativeDateModule, MatDatepickerModule, MAT_DATE_FORMATS, MatDialogModule, MatButtonModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
-import { EthAddressDirective } from './directives/eth-address/eth-address.directive';
-import { MatTabsModule } from '@angular/material/tabs';
-import { Web3Service, EtherscanUrlPipe, EthTokenValidatorDirective} from './services/web3/web3.service';
-import {ContractsService} from './services/contracts/contracts.service';
-import {UserService} from './services/user/user.service';
-import {UserInterface} from './services/user/user.interface';
-import {AuthComponent} from './common/auth/auth.component';
-import {AuthenticationComponent} from './common/auth/authentication/authentication.component';
-import {RegistrationComponent} from './common/auth/registration/registration.component';
-import {SocialComponent} from './common/auth/social/social.component';
-import {EmailConfirmComponent} from './common/auth/email-confirm/email-confirm.component';
-import {ForgotPasswordComponent} from './common/auth/forgot-password/forgot-password.component';
-import { ContractFormPayComponent } from './contract-form/contract-form-pay/contract-form-pay.component';
-import { ContractPreviewComponent } from './contract-preview/contract-preview.component';
-import { TransactionComponent } from './transaction/transaction.component';
-import {ContractsListComponent, ContractsListResolver} from './contracts-list/contracts-list.component';
-import { FooterComponent } from './footer/footer.component';
-import { PublicContractsComponent } from './index/public-contracts/public-contracts.component';
-import {ClipboardModule} from 'ngx-clipboard';
-import {BigNumberDirective, BigNumberFormat, BigNumberMax, BigNumberMin} from './directives/big-number/big-number.directive';
-import { ContactOwnerComponent } from './contact-owner/contact-owner.component';
-import { TeamComponent } from './team-component/team.component';
-import { RoadmapComponent } from './roadmap-component/roadmap.component';
-import { FaqComponent } from './faq-component/faq.component';
-import {MinMaxDirective} from './directives/minMax/min-max.directive';
-import { CookieService } from 'ngx-cookie-service';
-import { ContactsComponent } from './contacts-component/contacts.component';
-import { TokensAllInputComponent } from './directives/tokens-all-input/tokens-all-input.component';
-import {HttpService} from './services/http/http.service';
-import { ContractFormAllComponent, ContractEditResolver2 } from './contract-form-all/contract-form-all.component';
-import { ContractsPreviewV3Component } from './contracts-preview-v3/contracts-preview-v3.component';
-import { IndexIcoComponent } from './index-ico/index-ico.component';
-import { IndexIcoHeaderComponent } from './index-ico/index-ico-header/index-ico-header.component';
-import { IndexIcoFormComponent } from './index-ico/index-ico-form/index-ico-form.component';
-import {OwlModule} from 'ngx-owl-carousel';
-import {Observable} from 'rxjs';
-import {TransferHttpCacheModule} from '@nguniversal/common';
-import { CoinsListComponent } from './directives/coins-list/coins-list.component';
-import { ChangePasswordComponent } from './common/change-password/change-password.component';
-import { KeepInTouchComponent } from './footer/keep-in-touch/keep-in-touch.component';
-import { DateTransformPipe } from './contract-form-all/date-transform.pipe';
-import { ListFilterPipe } from './contract-form-all/list-filter.pipe';
-import { limitTo } from './contract-form-all/limit-to.pipe';
-import { FilterTokens } from './contract-form-all/filter-tokens.pipe';
-import {isPlatformBrowser, LOCATION_INITIALIZED} from "@angular/common";
-import {ActivatedRoute} from '@angular/router';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./index/header/header.component";
+import { StartFormComponent } from "./index/start-form/start-form.component";
+import { IndexComponent } from "./index/index.component";
+import { WillComponent } from "./will/index.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+  ContractEditResolver,
+  ContractFormComponent,
+} from "./contract-form/contract-form.component";
+import {
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MAT_DATE_FORMATS,
+  MatDialogModule,
+  MatButtonModule,
+} from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxMaterialTimepickerModule } from "ngx-material-timepicker";
+import { EthAddressDirective } from "./directives/eth-address/eth-address.directive";
+import { MatTabsModule } from "@angular/material/tabs";
+import {
+  Web3Service,
+  EtherscanUrlPipe,
+  EthTokenValidatorDirective,
+} from "./services/web3/web3.service";
+import { ContractsService } from "./services/contracts/contracts.service";
+import { UserService } from "./services/user/user.service";
+import { UserInterface } from "./services/user/user.interface";
+import { AuthComponent } from "./common/auth/auth.component";
+import { AuthenticationComponent } from "./common/auth/authentication/authentication.component";
+import { RegistrationComponent } from "./common/auth/registration/registration.component";
+import { SocialComponent } from "./common/auth/social/social.component";
+import { EmailConfirmComponent } from "./common/auth/email-confirm/email-confirm.component";
+import { ForgotPasswordComponent } from "./common/auth/forgot-password/forgot-password.component";
+import { ContractFormPayComponent } from "./contract-form/contract-form-pay/contract-form-pay.component";
+import { ContractPreviewComponent } from "./contract-preview/contract-preview.component";
+import { TransactionComponent } from "./transaction/transaction.component";
+import {
+  ContractsListComponent,
+  ContractsListResolver,
+} from "./contracts-list/contracts-list.component";
+import { FooterComponent } from "./footer/footer.component";
+import { PublicContractsComponent } from "./index/public-contracts/public-contracts.component";
+import { ClipboardModule } from "ngx-clipboard";
+import {
+  BigNumberDirective,
+  BigNumberFormat,
+  BigNumberMax,
+  BigNumberMin,
+} from "./directives/big-number/big-number.directive";
+import { ContactOwnerComponent } from "./contact-owner/contact-owner.component";
+import { TeamComponent } from "./team-component/team.component";
+import { RoadmapComponent } from "./roadmap-component/roadmap.component";
+import { FaqComponent } from "./faq-component/faq.component";
+import { MinMaxDirective } from "./directives/minMax/min-max.directive";
+import { CookieService } from "ngx-cookie-service";
+import { ContactsComponent } from "./contacts-component/contacts.component";
+import { TokensAllInputComponent } from "./directives/tokens-all-input/tokens-all-input.component";
+import { HttpService } from "./services/http/http.service";
+import {
+  ContractFormAllComponent,
+  ContractEditResolver2,
+} from "./contract-form-all/contract-form-all.component";
+import { ContractsPreviewV3Component } from "./contracts-preview-v3/contracts-preview-v3.component";
+import { IndexIcoComponent } from "./index-ico/index-ico.component";
+import { IndexIcoHeaderComponent } from "./index-ico/index-ico-header/index-ico-header.component";
+import { IndexIcoFormComponent } from "./index-ico/index-ico-form/index-ico-form.component";
+import { OwlModule } from "ngx-owl-carousel";
+import { Observable } from "rxjs";
+import { TransferHttpCacheModule } from "@nguniversal/common";
+import { CoinsListComponent } from "./directives/coins-list/coins-list.component";
+import { ChangePasswordComponent } from "./common/change-password/change-password.component";
+import { KeepInTouchComponent } from "./footer/keep-in-touch/keep-in-touch.component";
+import { DateTransformPipe } from "./contract-form-all/date-transform.pipe";
+import { ListFilterPipe } from "./contract-form-all/list-filter.pipe";
+import { limitTo } from "./contract-form-all/limit-to.pipe";
+import { FilterTokens } from "./contract-form-all/filter-tokens.pipe";
+import { isPlatformBrowser, LOCATION_INITIALIZED } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
 
 export class TranslateBrowserLoader implements TranslateLoader {
-
-  constructor(private prefix: string = 'i18n',
-              private suffix: string = '.json',
-              private transferState: TransferState,
-              private http: HttpClient) {
-  }
+  constructor(
+    private prefix: string = "i18n",
+    private suffix: string = ".json",
+    private transferState: TransferState,
+    private http: HttpClient
+  ) {}
 
   public getTranslation(lang: string): Observable<any> {
-
-    const key: StateKey<number> = makeStateKey<number>('transfer-translate-' + lang);
+    const key: StateKey<number> = makeStateKey<number>(
+      "transfer-translate-" + lang
+    );
     const data = this.transferState.get(key, null);
 
     // First we are looking for the translations in transfer-state, if none found, http load as fallback
     if (data) {
-      return Observable.create(observer => {
+      return Observable.create((observer) => {
         observer.next(data);
         observer.complete();
       });
     } else {
-      return new TranslateHttpLoader(this.http, this.prefix, this.suffix).getTranslation(lang);
+      return new TranslateHttpLoader(
+        this.http,
+        this.prefix,
+        this.suffix
+      ).getTranslation(lang);
     }
   }
 }
 
-export function exportTranslateStaticLoader(http: HttpClient, transferState: TransferState) {
-  return new TranslateBrowserLoader('./assets/i18n/', '.json?_t=' + (new Date).getTime(), transferState, http);
+export function exportTranslateStaticLoader(
+  http: HttpClient,
+  transferState: TransferState
+) {
+  return new TranslateBrowserLoader(
+    "./assets/i18n/",
+    ".json?_t=" + new Date().getTime(),
+    transferState,
+    http
+  );
 }
-
 
 // export function HttpLoaderFactory(http: HttpClient) {
 //   return new TranslateHttpLoader(http);
 // }
 
-export function appInitializerFactory(translate: TranslateService, userService: UserService, injector: Injector) {
-
-  const defaultLng = (navigator.language || navigator['browserLanguage']).split('-')[0];
+export function appInitializerFactory(
+  translate: TranslateService,
+  userService: UserService,
+  injector: Injector
+) {
+  const defaultLng = (navigator.language || navigator["browserLanguage"]).split(
+    "-"
+  )[0];
   console.log("LANG defaultLng", defaultLng);
 
-  const langToSet = window['jQuery']['cookie']('lng') || ((['en', 'zh', 'ko', 'ru'].indexOf(defaultLng) > -1) ? defaultLng : 'en');
+  const langToSet =
+    window["jQuery"]["cookie"]("lng") ||
+    (["en", "zh", "ko", "ru"].indexOf(defaultLng) > -1 ? defaultLng : "en");
   console.log("LANG langToSet", langToSet);
 
-  return () => new Promise<any>((resolve: any, reject) => {
+  return () =>
+    new Promise<any>((resolve: any, reject) => {
+      translate.setDefaultLang("en");
 
-    translate.setDefaultLang('en');
-    
-    translate.use(langToSet).subscribe(() => {
-      const subscriber = userService.getCurrentUser(true).subscribe((user: UserInterface) => {
-
-        subscriber.unsubscribe();
-        resolve(null);
-
+      translate.use(langToSet).subscribe(() => {
+        const subscriber = userService
+          .getCurrentUser(true)
+          .subscribe((user: UserInterface) => {
+            subscriber.unsubscribe();
+            resolve(null);
+          });
       });
     });
-
-  });
 }
 
 @NgModule({
@@ -168,14 +228,14 @@ export function appInitializerFactory(translate: TranslateService, userService: 
     DateTransformPipe,
     ListFilterPipe,
     limitTo,
-    FilterTokens
+    FilterTokens,
   ],
   entryComponents: [
     AuthComponent,
     TransactionComponent,
     ContactOwnerComponent,
     IndexIcoFormComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
   ],
   imports: [
     TransferHttpCacheModule,
@@ -184,14 +244,13 @@ export function appInitializerFactory(translate: TranslateService, userService: 
       loader: {
         provide: TranslateLoader,
         useFactory: exportTranslateStaticLoader,
-        deps: [HttpClient, TransferState]
-      }
-    }
-  ),
+        deps: [HttpClient, TransferState],
+      },
+    }),
 
     HttpClientXsrfModule.withOptions({
-      cookieName: 'csrftoken',
-      headerName: 'X-CSRFToken'
+      cookieName: "csrftoken",
+      headerName: "X-CSRFToken",
     }),
     MatDialogModule,
     BrowserModule,
@@ -205,7 +264,7 @@ export function appInitializerFactory(translate: TranslateService, userService: 
     BrowserAnimationsModule,
     NgxMaterialTimepickerModule,
     ClipboardModule,
-    OwlModule
+    OwlModule,
   ],
   providers: [
     CookieService,
@@ -215,12 +274,10 @@ export function appInitializerFactory(translate: TranslateService, userService: 
     {
       provide: APP_INITIALIZER,
       useFactory: appInitializerFactory,
-      deps: [
-        TranslateService, UserService, HttpService
-      ],
-      multi: true
-    }
+      deps: [TranslateService, UserService, HttpService],
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/index/header/header.component.ts
+++ src/app/index/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, Inject, OnInit, PLATFORM_ID, TemplateRef, ViewChild } from '@angular/core';
-import { isPlatformBrowser, Location } from '@angular/common';
-import {LangChangeEvent, TranslateService} from "@ngx-translate/core";
-import { UserService } from '../../services/user/user.service';
-import { UserInterface } from '../../services/user/user.interface';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { NavigationStart, Router } from '@angular/router';
+import {
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+  TemplateRef,
+  ViewChild,
+} from "@angular/core";
+import { isPlatformBrowser, Location } from "@angular/common";
+import { LangChangeEvent, TranslateService } from "@ngx-translate/core";
+import { UserService } from "../../services/user/user.service";
+import { UserInterface } from "../../services/user/user.interface";
+import { MatDialog, MatDialogRef } from "@angular/material";
+import { NavigationStart, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
   public isBrowser: any;
@@ -24,8 +31,8 @@ export class HeaderComponent implements OnInit {
   public currLanguage: string;
   public currentPath: string;
 
-  @ViewChild('logoutConfirmation') logoutConfirmation: TemplateRef<any>;
-  @ViewChild('headerPage') headerPage;
+  @ViewChild("logoutConfirmation") logoutConfirmation: TemplateRef<any>;
+  @ViewChild("headerPage") headerPage;
 
   private logoutConfirmationModal: MatDialogRef<any>;
   private logoutProgress: boolean;
@@ -39,29 +46,29 @@ export class HeaderComponent implements OnInit {
     activatedRoute: Location,
     translate: TranslateService
   ) {
-
-
     this.currentUser = this.userService.getUserModel();
-    this.userService.getCurrentUser().subscribe((userProfile: UserInterface) => {
-      this.currentUser = userProfile;
-    });
+    this.userService
+      .getCurrentUser()
+      .subscribe((userProfile: UserInterface) => {
+        this.currentUser = userProfile;
+      });
 
     this.isBrowser = isPlatformBrowser(platformId);
 
     if (this.isBrowser) {
       window.onscroll = () => {
-        const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        const scrolled =
+          window.pageYOffset || document.documentElement.scrollTop;
         this.pageScrolled = scrolled > 50;
       };
     }
 
-    document.getElementsByTagName('body')[0]['addEventListener'](
-      'mousedown',
-      (event) => {
+    document
+      .getElementsByTagName("body")[0]
+      ["addEventListener"]("mousedown", (event) => {
         this.openedMenu = false;
         this.userMenuOpened = false;
-      }
-    );
+      });
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
@@ -70,61 +77,53 @@ export class HeaderComponent implements OnInit {
       }
     });
 
-
     this.translator = translate;
     this.languagesList = [
       {
-        lng: 'en',
-        title: 'en',
-        active: true
+        lng: "en",
+        title: "en",
+        active: true,
       },
       {
-        lng: 'zh',
-        title: 'zh',
-        active: false
+        lng: "es",
+        title: "es",
+        active: false,
       },
       {
-        lng: 'ko',
-        title: 'ko',
-        active: false
+        lng: "ru",
+        title: "ru",
+        active: false,
       },
-      {
-        lng: 'ru',
-        title: 'ru',
-        active: false
-      }
     ];
-
 
     translate.onLangChange.subscribe((event: LangChangeEvent) => {
       this.setActiveLanguage(event);
     });
-    
+
     this.setActiveLanguage({
-      lang: translate.currentLang
+      lang: translate.currentLang,
     });
 
-    this.currentPath = activatedRoute.path().split('?')[0];
-
+    this.currentPath = activatedRoute.path().split("?")[0];
   }
 
   private setActiveLanguage(event) {
     if (this.currLanguage) {
       this.languagesList.map((lang) => {
-        if(lang['lng'] === this.currLanguage) lang['active'] = true;
-        else lang['active'] = false;
+        if (lang["lng"] === this.currLanguage) lang["active"] = true;
+        else lang["active"] = false;
         // return lang['lng'] === this.currLanguage;
       });
     }
     this.currLanguage = event.lang;
 
     if (this.isBrowser) {
-      window['jQuery']['cookie']('lng', this.currLanguage);
+      window["jQuery"]["cookie"]("lng", this.currLanguage);
     }
 
     this.languagesList.map((lang) => {
-      if(lang['lng'] === this.currLanguage) lang['active'] = true;
-      else lang['active'] = false;
+      if (lang["lng"] === this.currLanguage) lang["active"] = true;
+      else lang["active"] = false;
       // return lang['lng'] === this.currLanguage;
     });
     this.languagesList.sort((a, b) => {
@@ -132,19 +131,19 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-
   public toggleLanguage() {
     this.openedLngList = !this.openedLngList;
   }
 
-  
   public setLanguage(lng) {
     this.translator.use(lng);
   }
 
-
   public openAuth() {
-    this.userService.openAuthForm().then(() => { }, () => { });
+    this.userService.openAuthForm().then(
+      () => {},
+      () => {}
+    );
   }
 
   ngOnInit() {
@@ -155,20 +154,22 @@ export class HeaderComponent implements OnInit {
 
   public openLogoutConfirmation() {
     this.logoutConfirmationModal = this.dialog.open(this.logoutConfirmation, {
-      width: '480px',
-      panelClass: 'custom-dialog-container'
+      width: "480px",
+      panelClass: "custom-dialog-container",
     });
   }
 
   public logoutSuccess() {
     this.logoutProgress = true;
-    this.userService.logout().then(() => {
-      this.logoutConfirmationModal.close();
-    }).finally(() => {
-      this.logoutProgress = false;
-      this.currentUser.is_ghost = true;
-      window.location.reload();
-    });
+    this.userService
+      .logout()
+      .then(() => {
+        this.logoutConfirmationModal.close();
+      })
+      .finally(() => {
+        this.logoutProgress = false;
+        this.currentUser.is_ghost = true;
+        window.location.reload();
+      });
   }
-
 }
